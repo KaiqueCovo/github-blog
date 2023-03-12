a7c87dc5a1e598dd325a53fdbbc145ce26c315d4
Kaique Covo <kaique.kng@gmail.com>
2023-03-12 16:57:04 -0300

    build: change port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svgr(), react()],
+  server: {
+    port: 3000,
+  },
   resolve: {
     alias: [
       {
