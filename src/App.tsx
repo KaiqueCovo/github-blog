5f0ca724f47332252171fce986f95fc2dec6d7d8
Kaique Covo <kaique.kng@gmail.com>
2023-03-12 16:50:46 -0300

    feature: Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "styled-components";
+import { Header } from "./components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Header />
       <h1>Vite + React</h1>
 
       <GlobalStyle />
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 296px;
+
+  background-image: url("../../assets/cover.svg");
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
